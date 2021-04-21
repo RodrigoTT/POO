3e3f84f83f8c11e929ac1371706026ba568bbff1
RodrigoTT <34324115+RodrigoTT@users.noreply.github.com>
2021-04-21 18:02:45 -0300

    Add files via upload

---
+++ Resposta7.java
@@ -0,0 +1,4 @@
+Resposta: O diamante mortal da morte é um problema que acontece com as heranças, mais um problema parecido com herança multipla
+quando um classe está herdando mais de uma classe e as duas herdam da mesma super classe. Exempplo:
+Existe a super classe "Animal", Mamiferos e Oviparos herdam de animal, mas quando vamos criar a classe "hornitorrinco"
+ percebemos que ela precisa herdar tanto de Mamiferos e de Oviparos, gerando assim o diamante mortal da morte.
